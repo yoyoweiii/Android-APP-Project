0cb372276c2731e46435b45eac01e3c7447a6ecc
yoyoweiii <60588210+yoyoweiii@users.noreply.github.com>
2023-10-26 16:12:29 -0500

    Update drawingboard.py
    This board can be drew by random color
    And a button that can erase if needed

--- drawingboard.py
+++ drawingboard.py
@@ -1,17 +1,38 @@
+from random import random
 from kivy.app import App
-from kivy.graphics import Rectangle, Line
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.graphics import Color, Ellipse, Line
 
-class MyWidget(Widget):
+
+class MyPaintWidget(Widget):
+
     def on_touch_down(self, touch):
-        touch.ud['line'] = Line(points=(touch.x,touch.y))
-        self.canvas.add(touch.ud['line'])
+        color = (random(), 1, 1)
+        with self.canvas:
+            Color(*color, mode='hsv')
+            d = 30.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
 
     def on_touch_move(self, touch):
-        touch.ud['line'].points += [touch.x,touch.y]
+        touch.ud['line'].points += [touch.x, touch.y]
 
-class MyApp(App):
+
+class MyPaintApp(App):
+
     def build(self):
-        return MyWidget()
+        parent = Widget()
+        self.painter = MyPaintWidget()
+        clearbtn = Button(text='Clear')
+        clearbtn.bind(on_release=self.clear_canvas)
+        parent.add_widget(self.painter)
+        parent.add_widget(clearbtn)
+        return parent
 
-MyApp().run()
+    def clear_canvas(self, obj):
+        self.painter.canvas.clear()
+
+
+if __name__ == '__main__':
+    MyPaintApp().run()
